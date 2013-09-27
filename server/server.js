0e52537a604ce8c66b6f0974e9be2bb927bd46d5
OrgaChem <orga.chem.job@gmail.com>
2013-09-27 12:20:08 +0900

    Remove an unnecessary code

--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@
 
 var io = require('socket.io').listen(8080);
 
-var uid = 0;
-
 io.sockets.on('connection', function (socket) {
   console.log('connection');
   socket.on('message', function (msg) {
